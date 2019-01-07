c6ea9451adfeb962ad6428c1f41306366491588c
Felix Springer <felixspringer149@gmail.com>
2019-01-08 00:44:37 +0100

    scg function, backend call function, temp to gamma function, function structured

--- blugon.py
+++ blugon.py
@@ -15,6 +15,8 @@ if not CONFIG_DIR:
     CONFIG_DIR = getenv('HOME') + '/.config'
 CONFIG_DIR += '/blugon'
 
+BACKEND = 'scg'
+
 SIMULATE = False
 
 #-----------------------------------------------------------------
@@ -46,6 +48,122 @@ SIMULATE = args.simulate
 #-----------------------------------------------------------------
 
 MAX_MINUTE = 24 * 60
+
+KELVIN_MIN = 1000
+KELVIN_MAX = 12000
+KELVIN_STEP = 100
+KELVIN_RGB_TABLE = {
+    1000:  (255,  56,   0),
+    1100:  (255,  71,   0),
+    1200:  (255,  83,   0),
+    1300:  (255,  93,   0),
+    1400:  (255, 101,   0),
+    1500:  (255, 109,   0),
+    1600:  (255, 115,   0),
+    1700:  (255, 121,   0),
+    1800:  (255, 126,   0),
+    1900:  (255, 131,   0),
+    2000:  (255, 138,  18),
+    2100:  (255, 142,  33),
+    2200:  (255, 147,  44),
+    2300:  (255, 152,  54),
+    2400:  (255, 157,  63),
+    2500:  (255, 161,  72),
+    2600:  (255, 165,  79),
+    2700:  (255, 169,  87),
+    2800:  (255, 173,  94),
+    2900:  (255, 177, 101),
+    3000:  (255, 180, 107),
+    3100:  (255, 184, 114),
+    3200:  (255, 187, 120),
+    3300:  (255, 190, 126),
+    3400:  (255, 193, 132),
+    3500:  (255, 196, 137),
+    3600:  (255, 199, 143),
+    3700:  (255, 201, 148),
+    3800:  (255, 204, 153),
+    3900:  (255, 206, 159),
+    4000:  (255, 209, 163),
+    4100:  (255, 211, 168),
+    4200:  (255, 213, 173),
+    4300:  (255, 215, 177),
+    4400:  (255, 217, 182),
+    4500:  (255, 219, 186),
+    4600:  (255, 221, 190),
+    4700:  (255, 223, 194),
+    4800:  (255, 225, 198),
+    4900:  (255, 227, 202),
+    5000:  (255, 228, 206),
+    5100:  (255, 230, 210),
+    5200:  (255, 232, 213),
+    5300:  (255, 233, 217),
+    5400:  (255, 235, 220),
+    5500:  (255, 236, 224),
+    5600:  (255, 238, 227),
+    5700:  (255, 239, 230),
+    5800:  (255, 240, 233),
+    5900:  (255, 242, 236),
+    6000:  (255, 243, 239),
+    6100:  (255, 244, 242),
+    6200:  (255, 245, 245),
+    6300:  (255, 246, 247),
+    6400:  (255, 248, 251),
+    6500:  (255, 249, 253),
+    6600:  (254, 249, 255),
+    6700:  (252, 247, 255),
+    6800:  (249, 246, 255),
+    6900:  (247, 245, 255),
+    7000:  (245, 243, 255),
+    7100:  (243, 242, 255),
+    7200:  (240, 241, 255),
+    7300:  (239, 240, 255),
+    7400:  (237, 239, 255),
+    7500:  (235, 238, 255),
+    7600:  (233, 237, 255),
+    7700:  (231, 236, 255),
+    7800:  (230, 235, 255),
+    7900:  (228, 234, 255),
+    8000:  (227, 233, 255),
+    8100:  (225, 232, 255),
+    8200:  (224, 231, 255),
+    8300:  (222, 230, 255),
+    8400:  (221, 230, 255),
+    8500:  (220, 229, 255),
+    8600:  (218, 229, 255),
+    8700:  (217, 227, 255),
+    8800:  (216, 227, 255),
+    8900:  (215, 226, 255),
+    9000:  (214, 225, 255),
+    9100:  (212, 225, 255),
+    9200:  (211, 224, 255),
+    9300:  (210, 223, 255),
+    9400:  (209, 223, 255),
+    9500:  (208, 222, 255),
+    9600:  (207, 221, 255),
+    9700:  (207, 221, 255),
+    9800:  (206, 220, 255),
+    9900:  (205, 220, 255),
+    10000: (207, 218, 255),
+    10100: (207, 218, 255),
+    10200: (206, 217, 255),
+    10300: (205, 217, 255),
+    10400: (204, 216, 255),
+    10500: (204, 216, 255),
+    10600: (203, 215, 255),
+    10700: (202, 215, 255),
+    10800: (202, 214, 255),
+    10900: (201, 214, 255),
+    11000: (200, 213, 255),
+    11100: (200, 213, 255),
+    11200: (199, 212, 255),
+    11300: (198, 212, 255),
+    11400: (198, 212, 255),
+    11500: (197, 211, 255),
+    11600: (197, 211, 255),
+    11700: (197, 210, 255),
+    11800: (196, 210, 255),
+    11900: (195, 210, 255),
+    12000: (195, 209, 255)}
 
 #-----------------------------------------------------------------
 
@@ -84,19 +202,19 @@ def read_gamma():
     minutes = (list(map(pop_first, gamma)))
     return gamma, minutes
 
-def get_minute():
-    """returns the current minute"""
-    time_struct = time.localtime()
-    minute = 60 * time_struct.tm_hour + time_struct.tm_min + time_struct.tm_sec / 60
-    return minute
-
-def call_xgamma(red_gamma, green_gamma, blue_gamma):
-    """change screen color with backend xorg-xgamma"""
-    str_red_gamma = str(red_gamma)
-    str_green_gamma = str(green_gamma)
-    str_blue_gamma = str(blue_gamma)
-    check_call(['xgamma', '-quiet', '-rgamma', str_red_gamma, '-ggamma', str_green_gamma, '-bgamma', str_blue_gamma])
-    return
+def temp_to_gamma(temp):
+    """transforms temperature in kelvin to gamma values"""
+    if temp < KELVIN_MIN or temp > KELVIN_MAX:
+        raise ValueError('temperature values have to be between ' + str(KELVIN_MIN) + ' and ' + str(KELVIN_MAX))
+    temp_lower = KELVIN_STEP * int(math.floor(temp / KELVIN_STEP))
+    temp_upper = temp_lower + KELVIN_STEP
+    factor = (temp - temp_lower) / KELVIN_STEP
+    red_lower, green_lower, blue_lower = KELVIN_RGB_TABLE[temp_lower]
+    red_upper, green_upper, blue_upper = KELVIN_RGB_TABLE[temp_upper]
+    red_gamma = (factor * red_lower + (1 - factor) * red_upper) / 255
+    green_gamma = (factor * green_lower + (1 - factor) * green_upper) / 255
+    blue_gamma = (factor * blue_lower + (1 - factor) * blue_upper) / 255
+    return red_gamma, green_gamma, blue_gamma
 
 def calc_gamma(minute, list_minutes, list_gamma):
     """calculates the RGB gamma values"""
@@ -131,6 +249,35 @@ def calc_gamma(minute, list_minutes, list_gamma):
 
     return red_gamma, green_gamma, blue_gamma
 
+def call_xgamma(red_gamma, green_gamma, blue_gamma):
+    """start subprocess of backend xorg-xgamma"""
+    str_red_gamma = str(red_gamma)
+    str_green_gamma = str(green_gamma)
+    str_blue_gamma = str(blue_gamma)
+    check_call(['xgamma', '-quiet', '-rgamma', str_red_gamma, '-ggamma', str_green_gamma, '-bgamma', str_blue_gamma])
+    return
+
+def call_scg(red_gamma, green_gamma, blue_gamma):
+    """start subprocess of backend scg"""
+    str_red_gamma = str(red_gamma)
+    str_green_gamma = str(green_gamma)
+    str_blue_gamma = str(blue_gamma)
+    check_call(['scg', str_red_gamma, str_green_gamma, str_blue_gamma])
+    return
+
+def call_backend(backend, red_gamma, green_gamma, blue_gamma):
+    if backend == 'xgamma':
+        call_xgamma(red_gamma, green_gamma, blue_gamma)
+    elif backend == 'scg':
+        call_scg(red_gamma, green_gamma, blue_gamma)
+    return
+
+def get_minute():
+    """returns the current minute"""
+    time_struct = time.localtime()
+    minute = 60 * time_struct.tm_hour + time_struct.tm_min + time_struct.tm_sec / 60
+    return minute
+
 def reprint_time(minute):
     """prints time in a human readable format"""
     str_hour = ('00' + str(int(minute // 60)))[-2:]
@@ -146,7 +293,7 @@ def main():
     def while_body(minute, sleep_time=0):
         """puts everything together to have only one function to call"""
         red_gamma, green_gamma, blue_gamma = calc_gamma(minute, LIST_MINUTES, LIST_GAMMA)
-        call_xgamma(red_gamma, green_gamma, blue_gamma)
+        call_backend(BACKEND, red_gamma, green_gamma, blue_gamma)
         try:
             time.sleep(sleep_time)
         except:
