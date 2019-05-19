0fb4b6bb23f362d35a3dbdb3d6fe96fae3ee57ce
Felix Springer <felixspringer149@gmail.com>
2019-05-19 02:19:56 +0200

    shebangs from /usr/bin/... to /usr/bin/env ..., shellcheck on make-test.sh

--- backends/tty/tty.sh
+++ backends/tty/tty.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
 
 EscStart="\e]P"
 
--- test/make-test.sh
+++ test/make-test.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-TEST_DIR="`dirname "${BASH_SOURCE[0]}"`"
+TEST_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-cd "$TEST_DIR"
+cd "$TEST_DIR" || exit
 cd ..
 
 mkdir -p "test/build"
-MAKE_PREFIX="`pwd`/test/build"
+MAKE_PREFIX="$(pwd)/test/build"
 
 make PREFIX="$MAKE_PREFIX"
 make install PREFIX="$MAKE_PREFIX"
