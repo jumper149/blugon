8460f87ecd2c59d1d9d1236df845f19a2bad15ad
Devin J. Pohly <djpohly@gmail.com>
2023-04-13 10:19:16 -0500

    scg: allow selecting CRTC by an output name

--- backends/scg/scg.c
+++ backends/scg/scg.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/Xrandr.h>
 
@@ -15,9 +16,24 @@ double parse_arg(const char *arg) {
     return gamma;
 }
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
+int parse_int_arg(const char *arg) {
+    errno = 0;
+    int num = strtol(arg, NULL, 10);
+    if (errno) {
+        perror("strtol");
         exit(EXIT_FAILURE);
+    }
+
+    return num;
+}
+
+int main(int argc, char **argv) {
+    char *output = NULL;
+    if (argc < 4 || argc > 5) {
+        exit(EXIT_FAILURE);
+    }
+    if (argc >= 5) {
+        output = argv[4];
     }
 
     double gamma_r = parse_arg(argv[1]);
@@ -32,8 +48,24 @@ int main(int argc, char **argv) {
     Window root = XDefaultRootWindow(dpy);
     XRRScreenResources *res = XRRGetScreenResourcesCurrent(dpy, root);
 
+    RRCrtc output_crtc = 0;
+    if (output != NULL) {
+        for (int m = 0; m < res->noutput; m++) {
+            XRROutputInfo *oi = XRRGetOutputInfo(dpy, res, res->outputs[m]);
+            if (!strcmp(oi->name, output))
+                output_crtc = oi->crtc;
+            XRRFreeOutputInfo(oi);
+        }
+        if (!output_crtc) {
+            fprintf(stderr, "could not find crtc for output %s\n", output);
+            exit(EXIT_FAILURE);
+        }
+    }
+
     for (int c = 0; c < res->ncrtc; c++) {
         RRCrtc crtc = res->crtcs[c];
+        if (output_crtc && crtc != output_crtc)
+            continue;
         int size = XRRGetCrtcGammaSize(dpy, crtc);
         XRRCrtcGamma *crtc_gamma = XRRAllocGamma(size);
 
